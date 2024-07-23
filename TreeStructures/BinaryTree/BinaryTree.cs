cfb146965795224f978150408715a8dbdcf5a762
hongyichao <lucaschao@outlook.com>
2024-07-23 18:43:27 +1000

    init tree structure project

---
+++ TreeStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TreeStructures.BinaryTree
+{
+    public class BinaryTree
+    {
+        private Node root;
+
+        public void PrintByPreOrderTraverse() 
+        {
+            //root, left, right
+            TraversePreOrder(root);
+        }
+
+        private void TraversePreOrder(Node current) 
+        {
+            if (current == null)
+                return;
+            Console.WriteLine(current.Value);
+            TraversePreOrder(current.LeftChild);
+            TraversePreOrder(current.RightChild);
+        }
+
+
+        public void PrintByInOrderTraverse(bool isInDescendingOrder) 
+        {
+            if (!isInDescendingOrder)
+            {
+                //Left, Root, Right
+                TraverseInAscendingOrder(root);
+            }
+            else 
+            {
+                //Right, Root, Left
+                TraverseInDescendingOrder(root);
+            }
+        }
+
+        private void TraverseInAscendingOrder(Node current) 
+        {
+            if(current==null)
+                return;
+
+            TraverseInAscendingOrder(current.LeftChild);
+            Console.WriteLine(current.Value);
+            TraverseInAscendingOrder(current.RightChild);
+        }
+
+        private void TraverseInDescendingOrder(Node current)
+        {
+            if (current == null)
+                return;
+
+            TraverseInDescendingOrder(current.RightChild);            
+            Console.WriteLine(current.Value);
+            TraverseInDescendingOrder(current.LeftChild);
+        }
+
+
+        public void PrintByPostOrderTraverse() 
+        {
+            //Left, Right, Root
+            TraversePostOrder(root);
+        }
+
+        private void TraversePostOrder(Node current) 
+        { 
+            if(current == null)
+                return;
+
+            TraversePostOrder(current.LeftChild);
+            TraversePostOrder(current.RightChild);
+            Console.WriteLine(current.Value);
+        }
+
+        public bool Find(int value) 
+        {
+            var current = root;
+
+            while (current != null) 
+            {
+                if (current.Value == value)
+                    return true;
+                else {
+                    if (value > current.Value)
+                    {
+                        current = current.RightChild;
+                    }
+                    else 
+                    {
+                        current = current.LeftChild;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Insert(int value) 
+        {
+            var newNode = new Node(value);
+
+            if (root == null)
+            {
+                root = newNode;
+                return;
+            }
+            else 
+            {
+                var current = root;
+
+                while (true) 
+                {
+                    if (value < current.Value) 
+                    {
+                        if (current.LeftChild == null) 
+                        {
+                            current.LeftChild = newNode;
+                            break;
+                        }
+                        current = current.LeftChild;
+                    }
+                    else
+                    {
+                        if (current.RightChild == null) 
+                        {
+                            current.RightChild = newNode; 
+                            break;
+                        }
+                        current = current.RightChild;
+                    }
+                }
+            }
+
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+
+        public override string ToString()
+        {
+            return "Node value: " + Value;
+        }
+    }
+}
---
+++ TreeStructures/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using TreeStructures.BinaryTree;
+
+BinaryTree binaryTree = new BinaryTree();
+
+binaryTree.Insert(100);
+binaryTree.Insert(90);
+binaryTree.Insert(80);
+binaryTree.Insert(70);
+binaryTree.Insert(75);
+binaryTree.Insert(110);
+binaryTree.Insert(105);
+binaryTree.Insert(120);
+
+var searchResult = binaryTree.Find(75);
+Console.WriteLine(searchResult);
+
+searchResult = binaryTree.Find(110);
+Console.WriteLine(searchResult);
+
+searchResult = binaryTree.Find(200);
+Console.WriteLine(searchResult);
+
+Console.WriteLine();
+Console.WriteLine("PrintByPreOrderTraverse");
+binaryTree.PrintByPreOrderTraverse();
+
+Console.WriteLine();
+Console.WriteLine("PrintByInOrderTraverse");
+binaryTree.PrintByInOrderTraverse(false);
+
+Console.WriteLine();
+Console.WriteLine("PrintByInOrderTraverse");
+binaryTree.PrintByInOrderTraverse(true);
+
+Console.WriteLine();
+Console.WriteLine("PrintByPostOrderTraverse");
+binaryTree.PrintByPostOrderTraverse();
+
+
+Console.WriteLine("done");
