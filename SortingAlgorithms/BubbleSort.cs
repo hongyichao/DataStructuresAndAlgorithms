2077e59fd0fc1650aa1a6986702c5540ffc20cec
hongyichao <lucaschao@outlook.com>
2024-08-14 17:04:11 +1000

    Sorting Algorithm BUbble Sort

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] items) 
+        {
+            bool isSorted;
+
+            for (int i = 0; i < items.Length - 1; i++)
+            {
+                isSorted = true;
+
+                for (int j = 0; j < items.Length - 1 - i; j++)
+                {
+                    if (items[j] > items[j + 1])
+                    {
+                        var tmp = items[j];
+                        items[j] = items[j + 1];
+                        items[j + 1] = tmp;
+
+                        isSorted = false;
+                    }
+                }
+
+                if(isSorted)
+                    return items;
+            }
+
+            return items;
+        }
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SortingAlgorithms;
+
+Console.WriteLine("Hello, World!");
+
+
+var items = new int[] { 5, 4, 3, 2, 1 };
+
+BubbleSort bubbleSort = new BubbleSort();
+
+bubbleSort.Sort(items);
+
+Console.WriteLine(string.Join(", ", items));
+
+items = new int[] { 3, 4, 5, 6, 7 };
+bubbleSort.Sort(items);
+
+Console.WriteLine(string.Join(", ", items));
