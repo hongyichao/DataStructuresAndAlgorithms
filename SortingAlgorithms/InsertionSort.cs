216fe4f4a9908977cc44dd5cff8ac83ea41e84ad
hongyichao <lucaschao@outlook.com>
2024-08-16 11:18:44 +1000

    insertion sort

---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class InsertionSort
+    {
+        public int[] Sort(int[] items) 
+        {
+            for (int i = 1; i < items.Length; i++) 
+            {
+                var currentIndex = i;
+                var currentValue = items[i];
+                for (int j = i - 1; j >= 0 ; j--) 
+                {
+                    if (currentValue < items[j])
+                    { 
+                        items[currentIndex] = items[j];
+                        currentIndex--;
+                    }
+                }
+
+                items[currentIndex] = currentValue;
+            }
+
+            return items;
+        }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -26,3 +26,18 @@ SelectionSort selectionSort = new SelectionSort();
 var result = selectionSort.Sort(items);
 
 Console.WriteLine(string.Join(", ", result));
+
+InsertionSort insertionSort = new InsertionSort();
+
+
+items = new int[] { 5, 4, 3, 2, 1 };
+var result2 =  insertionSort.Sort(items);
+Console.WriteLine(string.Join(", ", result2));
+
+items = new int[] { 1, 1, 4, 7, 5, 4, 2 };
+result2 = insertionSort.Sort(items);
+Console.WriteLine(string.Join(", ", result2));
+
+items = new int[] { 7, 5};
+result2 = insertionSort.Sort(items);
+Console.WriteLine(string.Join(", ", result2));
