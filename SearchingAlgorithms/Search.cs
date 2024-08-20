5f832e8c568183cdfe87d4967d5eb877f98504f8
hongyichao <lucaschao@outlook.com>
2024-08-20 14:54:00 +1000

    Jump search

--- SearchingAlgorithms/Search.cs
+++ SearchingAlgorithms/Search.cs
@@ -92,5 +92,28 @@ namespace SearchingAlgorithms
 
             return TernarySearch(array, target, mid1 + 1, mid2 - 1);
         }
+
+        public int JumpSearch(int[] array, int target) 
+        {
+            var blockSize= (int)Math.Sqrt(array.Length);
+            int start = 0;
+            int next = blockSize;
+
+            while (start < array.Length && array[next - 1] < target) 
+            {
+                start = next;
+                next += blockSize;
+                if(next > array.Length)
+                    next = array.Length;
+            }
+
+            for (int i = start; i < next; i++) 
+            {
+                if (array[i] == target)
+                    return i;
+            }
+
+            return -1;
+        }
     }
 }
